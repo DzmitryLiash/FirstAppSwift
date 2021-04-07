97053f8cd4b826f35227fd517fa6bbcba6ddfabc
Dzmitry Liashchou <liashchou.d@gmail.com>
2021-04-08 00:56:03 +0200

    Second commit - Main.Storyboard

--- HW/ViewController.swift
+++ HW/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var helloWorldLabel: UILabel!
+    
+    @IBOutlet var tapHereButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        tapHereButton.layer.cornerRadius = 5
     }
 
-
+    @IBAction func TupHereButtonPressed() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            tapHereButton.setTitle("CLEAR TEXT", for: .normal)
+            
+        } else {
+            helloWorldLabel.isHidden = true
+            tapHereButton.setTitle("SHOW TEXT", for: .normal)
+        }
+        
+        
+        
+    }
+    
 }
 
